2a6dc8bbbc3e40bcb5b72e7258864300090541d0
Alexander Goida <alexander.goida@gmail.com>
2016-05-24 14:02:37 +0300

    Project for examples

---
+++ Configuration.Autofac/ImpementationModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace LifetimeScopesExamples.Implementation.Autofac
+{
+    public class ImpementationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
