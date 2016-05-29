fa510e2e15aa681d018e1bd948b5153ca3578a4e
Alexander Goida <alexander.goida@gmail.com>
2016-05-29 13:04:50 +0300

    Ninject registrations completed

--- Implementation/Configuration/Ninject/Configuration.cs
+++ Implementation/Configuration/Ninject/Configuration.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using LifetimeScopesExamples.Abstraction;
+﻿using LifetimeScopesExamples.Abstraction;
 using LifetimeScopesExamples.Implementation.Repositories.Constructors;
-using LifetimeScopesExamples.Implementation.Repositories.Properties;
 using LifetimeScopesExamples.Implementation.Repositories.Methods;
+using LifetimeScopesExamples.Implementation.Repositories.Properties;
 using Ninject;
+using Ninject.Extensions.Conventions;
 
 namespace LifetimeScopesExamples.Implementation.Configuration.Ninject
 {
@@ -25,64 +25,38 @@ namespace LifetimeScopesExamples.Implementation.Configuration.Ninject
             container.Bind<IAuthorRepository>().ToConstructor(x => new AuthorRepositoryCtro(x.Inject<ILog>(), x.Inject<IBookRepository>()));
             container.Bind<IBookRepository>().ToConstructor(x => new BookRepositoryCtro(x.Inject<ILog>()));
             container.Bind<ILog>().ToConstructor(x => new ConsoleLog());
+            //container.Bind<IAuthorRepository>().ToMethod(x => new AuthorRepositoryCtro(x.Kernel.Get<ILog>(), x.Kernel.Get<IBookRepository>()));
+            //container.Bind<IBookRepository>().ToMethod(x => new BookRepositoryCtro(x.Kernel.Get<ILog>()));
+            //container.Bind<ILog>().ToMethod(x => new ConsoleLog());
 
             return new DependencyResolver(container);
         }
 
         public static IDependencyResolver Properties()
         {
-            throw new NotImplementedException();
-            //var container = new Container();
-            //container.Configure(c =>
-            //{
-            //    c.For<IAuthorRepository>().Use<AuthorRepositoryCtro>();
-            //    c.For<IBookRepository>().Use<BookRepositoryCtro>();
-            //    c.For<ILog>().Use(() => new ConsoleLog());
-            //    c.Policies.SetAllProperties(convention => { });
-            //});
-            //return new DependencyResolver(container);
+            var container = new StandardKernel();
+            container.Bind<IAuthorRepository>().To<AuthorRepositoryProp>();
+            container.Bind<IBookRepository>().To<BookRepositoryProp>();
+            container.Bind<ILog>().To<ConsoleLog>();
+
+            return new DependencyResolver(container);
         }
 
         public static IDependencyResolver Methods()
         {
-            //todo structuremap method injection
-            throw new NotImplementedException();
-            //var container = new Container();
-            //container.Configure(c =>
-            //{
-            //    c.For<IAuthorRepository>().Use(cntr =>
-            //    {
-            //        var rep = new AuthorRepositoryMtd();
-            //        rep.SetDependencies(container.GetInstance<ILog>(), container.GetInstance<IBookRepository>());
-            //        return rep;
-            //    });
-            //    c.For<IBookRepository>().Use(cntr =>
-            //    {
-            //        var rep = new BookRepositoryMtd();
-            //        rep.SetLog(container.GetInstance<ILog>());
-            //        return rep;
-            //    });
-            //    c.For<ILog>().Use(() => new ConsoleLog());
-            //});
+            var container = new StandardKernel();
+            container.Bind<IAuthorRepository>().To<AuthorRepositoryMtd>().OnActivation((context, mtd) => mtd.SetDependencies(context.Kernel.Get<ILog>(), context.Kernel.Get<IBookRepository>()));
+            container.Bind<IBookRepository>().To<BookRepositoryMtd>().OnActivation((context, mtd) => mtd.SetLog(context.Kernel.Get<ILog>()));
+            container.Bind<ILog>().To<ConsoleLog>();
 
-            //return new DependencyResolver(container);
+            return new DependencyResolver(container);
         }
 
         public static IDependencyResolver Auto()
         {
-            throw new NotImplementedException();
-            /*
-             * Make sure the implementation classes are PUBLIC
-             */
-            //var container = new Container();
-            //container.Configure(c => c.Scan(x =>
-            //{
-            //    x.TheCallingAssembly();
-            //    //x.With(new FirstInterfaceConvention());
-            //    x.RegisterConcreteTypesAgainstTheFirstInterface();
-            //}));
-
-            //return new DependencyResolver(container);
+            var container = new StandardKernel();
+            container.Bind(x => x.FromThisAssembly().SelectAllClasses().BindDefaultInterfaces());
+            return new DependencyResolver(container);
         }
 
         private class DependencyResolver : IDependencyResolver
